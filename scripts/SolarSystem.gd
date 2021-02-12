496712d59baa31675ef85d5303faea3677e7d8a9
rr <rdssdf.fd@gmx.at>
2021-02-13 00:51:55 +0100

    replace 8k milky way 360 degree panorama background with a simple noise texture. my pc can't handle the 8k :/

--- scripts/SolarSystem.gd
+++ scripts/SolarSystem.gd
@@ -1,8 +1,5 @@
 extends Spatial
 
-var _n_vertecies := 50
-var orbit_shader = preload("res://shaders/orbit.shader")
-
 func _ready():
 	var builder = OrbitBuilder.new()
 	var satellites = get_tree().get_nodes_in_group(Groups.SATELLITES)
