2cd1f044937899e702e3ab73553744042b4b86fb
rr <rdssdf.fd@gmx.at>
2021-02-12 03:30:28 +0100

    remove spawner node after spawning star system

--- scripts/StarSystemSpawner.gd
+++ scripts/StarSystemSpawner.gd
@@ -1,15 +1,26 @@
 extends Node
 
-func _input(_event):
-#	get_tree().change_scene("res://scenes/EmptyStarSystem.tscn")
-	if Input.is_action_just_pressed("ui_accept"):
-		get_node("InstructionsLabel").visible = false
+#var spawned = false
+#signal toggle_pause
+
+func _input(event):
+#	if not spawned:
+	if event.is_pressed():
+		get_node("InstructionsLabel").set_visible(false)
 		var def = generate_starsystem()
 		var scene = load("res://scenes/EmptyStarSystem.tscn").instance()
 		var builder = StarSystemBuilder.new()
 		builder.buildScene(scene, def)
 		get_tree().root.add_child(scene)
 		get_tree().current_scene = scene
+
+#			spawned = true
+		# remove this node after switching to star system
+		queue_free()
+#	else:
+#		if event is InputEventKey and event.is_pressed() and event.scancode == KEY_SPACE:
+#			var satellites = get_tree().get_nodes_in_group(Groups.SATELLITES)
+
 
 func generate_starsystem():
 	var generator = StarSystemGenerator.new()
