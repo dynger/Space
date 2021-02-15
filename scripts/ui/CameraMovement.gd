b2db710b888d3d681c5c6123d8f806e61ba93521
h00g3 <h00g3@live.at>
2021-02-15 07:05:52 +0100

    Added free move camera (WASDQE) and telescope Mode

--- scripts/ui/CameraMovement.gd
+++ scripts/ui/CameraMovement.gd
@@ -1,73 +1,103 @@
-extends Camera
+class_name CameraMovement extends Camera
 
-class_name CameraMovement
-
-export var rotation_speed = 0.002
-export var move_speed = 100.0
-export var zoom_speed = 0.1
-onready var initial_rotation := rotation.y
-var mouse_middle_pressed = false
-var shift_pressed = false
-var motion = Vector3()
-var zoom_count = ZoomCount.new()
+export(float, 0.0, 1.0) var sensitivity = 0.1
 
 signal camera_transformed
 
-func _input(event: InputEvent) -> void:
+# Mouse state
+var _mouse_position = Vector2(0.0, 0.0)
+var _total_pitch = 0.0
+
+# Toggle Telescope
+var telescope_pressed = false
+
+# Movement state
+var _direction = Vector3(0.0, 0.0, 0.0)
+var _velocity = Vector3(0.0, 0.0, 0.0)
+var _acceleration = 30
+var _deceleration = -10
+var _vel_multiplier = 8
+
+# Keyboard state
+var _w = false
+var _s = false
+var _a = false
+var _d = false
+var _q = false
+var _e = false
+
+func _input(event):
+	# Receives mouse motion
+	if event is InputEventMouseMotion:
+		_mouse_position = event.relative
+
 	if event is InputEventMouseButton:
-		var button_index = event.get_button_index()
-		if button_index == BUTTON_MIDDLE:
-			mouse_middle_pressed = event.is_pressed()
-		elif button_index == BUTTON_WHEEL_UP:
-			motion.z -= zoom_speed
-		elif button_index == BUTTON_WHEEL_DOWN:
-			motion.z += zoom_speed
+		match event.button_index:
+			BUTTON_RIGHT: # Only allows rotation if right click down
+				Input.set_mouse_mode(Input.MOUSE_MODE_CAPTURED if event.pressed else Input.MOUSE_MODE_VISIBLE)
+			BUTTON_WHEEL_UP: # Increases max velocity
+				_vel_multiplier = clamp(_vel_multiplier * 1.1, 0.2, 20)
+			BUTTON_WHEEL_DOWN: # Decereases max velocity
+				_vel_multiplier = clamp(_vel_multiplier / 1.1, 0.2, 20)
 
-	if event is InputEventKey and event.get_scancode() == KEY_SHIFT:
-		shift_pressed = event.is_pressed()
+	if event is InputEventKey:
+		match event.scancode:
+			KEY_W:
+				_w = event.pressed
+			KEY_S:
+				_s = event.pressed
+			KEY_A:
+				_a = event.pressed
+			KEY_D:
+				_d = event.pressed
+			KEY_Q:
+				_q = event.pressed
+			KEY_E:
+				_e = event.pressed
 
-	if mouse_middle_pressed:
-		if event is InputEventMouseMotion:
-			if shift_pressed:
-				motion.x = -event.relative.x
-				motion.y = event.relative.y
-				motion = motion.normalized()
-			else:
-				# Horizontal mouse look
-				rotation.y -= event.relative.x * rotation_speed
-				# Vertical mouse look, clamped to -90..90 degrees
-				rotation.x = clamp(rotation.x - event.relative.y * rotation_speed, deg2rad(-90), deg2rad(90))
-				emit_signal("camera_transformed")
+func _process(delta):
+	_update_mouselook(delta)
+	_update_movement(delta)
 
+func _update_movement(delta):
+	_direction = Vector3(_d as float - _a as float, 
+						 _e as float - _q as float,
+						 _s as float - _w as float)
+	
+	var offset = _direction.normalized() * _acceleration * _vel_multiplier * delta \
+		+ _velocity.normalized() * _deceleration * _vel_multiplier * delta
+	
+	if _direction == Vector3.ZERO and offset.length_squared() > _velocity.length_squared():
+		_velocity = Vector3.ZERO
 	else:
-		clear_motion_xy()
-
-	if not shift_pressed:
-		clear_motion_xy()
-
-func clear_motion_xy() -> void:
-	motion.x = 0
-	motion.y = 0
-
-func _process(delta) -> void:
-	if motion != Vector3.ZERO:
-		translate(motion * move_speed * delta)
+		_velocity.x = clamp(_velocity.x + offset.x, -_vel_multiplier, _vel_multiplier)
+		_velocity.y = clamp(_velocity.y + offset.y, -_vel_multiplier, _vel_multiplier)
+		_velocity.z = clamp(_velocity.z + offset.z, -_vel_multiplier, _vel_multiplier)
+	
+		translate(_velocity * delta)
 		emit_signal("camera_transformed")
 
+func _update_mouselook(delta):
+	if Input.get_mouse_mode() == Input.MOUSE_MODE_CAPTURED:
+		_mouse_position *= sensitivity
+		var yaw = _mouse_position.x
+		var pitch = _mouse_position.y
+		_mouse_position = Vector2(0, 0)
+		
+		# prevents looking up/down beyond comprehension
+		pitch = clamp(pitch, -90 - _total_pitch, 90 - _total_pitch)
+		_total_pitch += pitch
+	
+		rotate_y(deg2rad(-yaw))
+		rotate_object_local(Vector3(1,0,0), deg2rad(-pitch))
 
-	if motion.z != 0:
-		var limit_reached = zoom_count.zooming(delta)
-		if limit_reached:
-			motion.z = 0
-			zoom_count.reset_count()
 
-class ZoomCount:
-	var count = 0
-	var limit = 0.3
-
-	func zooming(delta) -> bool:
-		count += delta
-		return count >= limit
-
-	func reset_count():
-		count = 0
+func _on_Button_toggled(button_pressed):
+	
+	if !telescope_pressed:
+		fov = 4
+		telescope_pressed = true
+	else:
+		fov = 60
+		telescope_pressed = false
+	
